414133211fe4a6359274459fa7ad556d3b7dc151
keithchasen <keithchasen89@gmail.com>
2024-10-07 21:05:05 +0200

    basic styles for canvas / resize canvas

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ console.log('game');
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const application = new Application();
 application.switchScreen('menu');
 
@@ -13,3 +16,8 @@ const animate = () => {
 };
 
 // animate();
+
+window.addEventListener('resize', () => {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+});
